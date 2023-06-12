e0c74f141be75f473e5432a6c1ec1e0a6b41bcf6
Shane Patrick <spp935@gmail.com>
2023-06-12 15:42:59 +0100

    refactor search tests

---
+++ product/constants.ts
@@ -0,0 +1,6 @@
+export const URL_TO_BE_ADDED = 'URL_TO_BE_ADDED'; // replace with actual URL
+
+export const SEARCH_BAR_SELECTOR = '.search-bar';
+export const PRODUCT_TITLE_SELECTOR = '.product-title';
+export const SEARCH_RESULTS_SELECTOR = '.search-results';
+export const ERROR_MESSAGE_SELECTOR = '.error-message';
